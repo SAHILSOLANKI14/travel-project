4d336b0a8ea03b522e6eeb5f1ef18d71ec6cbcff
Smit Solanki <smit.solanki.1010@gmail.com>
2024-04-13 16:40:35 +0530

    building login form

--- react-pro/my-app/src/Pages/SignIn.js
+++ react-pro/my-app/src/Pages/SignIn.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import LoginForm from './component/Assets/LoginForm'
 
 const SignIn = () => {
   return (
     <div style={{display:'flex' , justifyContent:'center',alignItems:'center',height:'70vh'}}>
-    <h1 style={{color:'black'}}>Sign-in</h1>
+    <LoginForm/>
   </div>
   )
 }
